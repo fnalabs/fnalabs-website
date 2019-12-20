f52a1a2338dc9fc66d587a3331243cadbf57ef41
Adam Eilers <adam.eilers@gmail.com>
2019-12-19 20:21:51 -0600

    updating website with a service worker and improved critical css

---
+++ docs/fnalabs-manifest.27abcdc4ef11aa28c17734fe5acc0daa.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29e91743ed7e4ab9f6e0838b3c20442c",
+    "url": "icon_114x114.png"
+  },
+  {
+    "revision": "f50706bc707b4ef692f1a5c70d7f386a",
+    "url": "icon_144x144.png"
+  },
+  {
+    "revision": "c7ac459dd6abf8ad62b950690430bc91",
+    "url": "icon_192x192.png"
+  },
+  {
+    "revision": "f9782bd1914b3fccbda2d3b658db731e",
+    "url": "icon_32x32.png"
+  },
+  {
+    "revision": "961e58155d9224ea2f9508169c3e6e50",
+    "url": "icon_512x512.png"
+  },
+  {
+    "revision": "efd4346a410aa8dfb5c71df2082538d9",
+    "url": "icon_57x57.png"
+  },
+  {
+    "revision": "a9d427618c80db769c14154bf8099efd",
+    "url": "icon_72x72.png"
+  },
+  {
+    "revision": "8dfafe9919b5178e15c5",
+    "url": "main.b8412b6f.js"
+  },
+  {
+    "revision": "15d20329eaf43503c5029c7dfb142abd",
+    "url": "main.b8412b6f.js.LICENSE"
+  },
+  {
+    "revision": "8dfafe9919b5178e15c5",
+    "url": "main.d28eeec9.css"
+  },
+  {
+    "revision": "8c2c77c9b5d901e90d5b89b8dbc22b0b",
+    "url": "manifest.json"
+  }
+]);
---
+++ docs/sw.js
@@ -0,0 +1,79 @@
+importScripts("fnalabs-manifest.27abcdc4ef11aa28c17734fe5acc0daa.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+/* eslint-env serviceworker */
+/* global workbox */
+
+// constants
+const urls = [
+  { url: '.' },
+  { url: '/projects' },
+  { url: '/about' },
+  { url: '/support' },
+  { url: '/cookie' },
+  { url: '/privacy' }
+]
+const precacheManifest = !self.__precacheManifest
+  ? urls
+  : self.__precacheManifest.reduce((ret, val) => {
+    if (val.revision) for (const url of urls) url.revision = val.revision
+    ret.push(val)
+    return ret
+  }, urls)
+
+// event handlers
+self.addEventListener('install', (event) => self.skipWaiting())
+
+// config
+workbox.core.setCacheNameDetails({
+  prefix: 'fnalabs'
+})
+workbox.googleAnalytics.initialize({
+  parameterOverrides: {
+    cd1: 'offline'
+  }
+})
+
+// precache and routing
+workbox.precaching.precacheAndRoute(precacheManifest)
+workbox.precaching.cleanupOutdatedCaches()
+
+workbox.routing.registerRoute(
+  /.*google-analytics\.com/,
+  new workbox.strategies.CacheFirst({
+    cacheName: 'fnalabs-analytics',
+    plugins: [
+      new workbox.expiration.Plugin({
+        maxEntries: 60,
+        maxAgeSeconds: 2592000 // 30 Days
+      })
+    ]
+  })
+)
+workbox.routing.registerRoute(
+  /\.(?:png|jpg|jpeg|svg|gif)$/,
+  new workbox.strategies.CacheFirst({
+    cacheName: 'fnalabs-images',
+    plugins: [
+      new workbox.expiration.Plugin({
+        maxEntries: 60,
+        maxAgeSeconds: 2592000 // 30 Days
+      })
+    ]
+  })
+)
+workbox.routing.registerRoute(
+  new RegExp('^https://fnalabs\\.github\\.io/fnalabs-assets/assets/'),
+  new workbox.strategies.CacheFirst({
+    cacheName: 'fnalabs-images',
+    plugins: [
+      new workbox.cacheableResponse.Plugin({
+        statuses: [0, 200]
+      }),
+      new workbox.expiration.Plugin({
+        maxEntries: 60,
+        maxAgeSeconds: 2592000 // 30 Days
+      })
+    ]
+  })
+)
+
